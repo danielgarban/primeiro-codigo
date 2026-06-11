efccd8a607ec81ba00d9f6227f960734d64fac75
Angel Daniel <angel_d_pino@estudante.sesisenai.org.br>
2026-06-10 21:51:40 -0300

    par é impar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Escreva a mensagem "Informe um numero:"
+Console.Write("Informe um numero:");
+// Cria a variavelQueSalvaUmNumero,
+ // Leio que o usuario digitar
+ // e transforma ovalor lido de texto para inteiro (int.parse)
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+
+// verifica se o valor de variavelQueSalvaUmNumero é par
+if (variavelQueSalvaUmNumero % 2 == 0){
+    Console.WriteLine($"Nuemro { variavelQueSalvaUmNumero} é par");
+}else {
+     Console.WriteLine($"Nuemro { variavelQueSalvaUmNumero} é impar");
+}
